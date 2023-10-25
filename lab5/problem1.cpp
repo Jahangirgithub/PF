b026f3fca152cf7c7d0782da02cbc0902786bc95
Jahangirgithub <jahangirakbar015@gmail.com>
2023-10-25 11:17:26 +0500

    Add new File

--- lab5/problem1.cpp
+++ lab5/problem1.cpp
@@ -1,5 +1,5 @@
 /*Problem1:
-Write a program that counts and displays the even and odd numbers. The program prompts the user to enter positive integers and terminate the process by entering -90. 
+Write a program that counts and displays the even and odd numbers. The program prompts the user to enter positive integers and terminate the process by entering -90.
 
 	Example:
 	The user enters:  4 15 1 8 10 6 -90
@@ -8,3 +8,29 @@ Write a program that counts and displays the even and odd numbers. The program p
 		Count of Positive Odd numbers:  2
 
 */
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number{}, even_counter{}, odd_counter{};
+	cout << "Enter the positive integers : ";
+	cin >> number;
+	while (number != -90)
+	{
+		if (number > 0)
+		{
+			if (number % 2 == 0)
+			{
+				even_counter += 1;
+			}
+			else
+			{
+				odd_counter += 1;
+			}
+		}
+		cin >> number;
+	}
+	cout << "Count of Positive Even numbers: " << even_counter << endl;
+	cout << "Count of Positive Odd numbers:  " << odd_counter << endl;
+	return 0;
+}
